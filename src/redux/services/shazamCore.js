eaf9cc97364ca7c1601408f000a3e3d91782578d
harshdeswal <harshdeswal03@yahoo.com>
2023-02-25 17:51:43 +0530

    done with the top songs,top artist

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,21 +1,6 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import axios from 'axios';
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://shazam.p.rapidapi.com/charts/list',
-//     headers: {
-//       'X-RapidAPI-Key': 'f68553add8msh5200b60a4c40c58p1f6d34jsn05b0ac15e397',
-//       'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-//     }
-//   };
-  
-//   axios.request(options).then(function (response) {
-//       console.log(response.data);
-//   }).catch(function (error) {
-//       console.error(error);
-//   });
-
   export const shazamCoreApi = createApi({
     reducerPath:'shazamCoreApi',
     baseQuery:fetchBaseQuery({
